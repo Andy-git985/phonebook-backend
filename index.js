9e1e14097fb5eb2ff6a7327bd8756cdc7cffa421
Andy-git985 <andy.yu617@gmail.com>
2022-10-06 18:06:57 -0400

    3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -2,8 +2,16 @@
 const { request, response } = require('express');
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body);
+});
 
 app.use(express.json());
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
 
 let persons = [
   {
