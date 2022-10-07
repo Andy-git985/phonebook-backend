3bb3ab592c4ed9af446a629d740f7ae903679d50
Andy-git985 <andy.yu617@gmail.com>
2022-10-06 21:46:13 -0400

    3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-// a: Node.js and Express -Receiving data
-const { request, response } = require('express');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const morgan = require('morgan');
 
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body);
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
@@ -98,6 +98,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
