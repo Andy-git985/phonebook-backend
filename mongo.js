aa997bae9b993409f4d386050b57bddbf0f5e20d
Andy-git985 <andy.yu617@gmail.com>
2022-10-07 00:10:30 -0400

    Exercise 3.12.

---
+++ mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log(
+    'Please provide the password as an argument: node mongo.js <password>'
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://GiW7P7wUXzY5mGxJ46:${password}@cluster0.1j5wdmv.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+mongoose.connect(url).catch((err) => console.log(err));
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    console.log('phonebook:');
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+    process.exit(0);
+  });
+}
+
+const name = process.argv[3];
+const number = process.argv[4];
+
+if (name || number) {
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
