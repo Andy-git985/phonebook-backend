335fd4745accf5e4cd05b55fd67f318f5cef55cc
Andy-git985 <andy.yu617@gmail.com>
2022-10-07 20:17:18 -0400

    3.13: Phonebook database, step1

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
+const Person = require('./models/person');
 
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body);
@@ -38,7 +40,9 @@ let persons = [
 ];
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get('/api/persons/:id', (request, response) => {
